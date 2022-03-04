4a4c5e89e75906b90a0614c31599b371a5fc124c
Násser Saleh <nasser.saleh@usp.br>
2022-03-04 13:15:13 -0300

    add: button-style

---
+++ javascript/index.js
@@ -0,0 +1,8 @@
+async function getData(){
+    const response = await fetch("https://api.json-generator.com/templates/BQZ3wDrI6ts0/data?access_token=n7lhzp6uj5oi5goj0h2qify7mi2o8wrmebe3n5ad");
+    const pius = await response.json();
+    
+    console.log(pius)
+}
+getData()
+
